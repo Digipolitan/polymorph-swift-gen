62830f9df5ccec7eccf408858a80c690973df0b8
Benoit BRIATTE <briatte.benoit@digipolitan.com>
2017-08-16 11:33:46 +0200

    remove writer from this project -> move to SwiftCodeWriter project

--- Package.swift
+++ Package.swift
@@ -14,13 +14,14 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         .package(url: "https://github.com/Digipolitan/polymorph-gen-swift.git", .branch("develop")),
+        .package(url: "https://github.com/Digipolitan/swiftcode-writer-swift.git", .branch("develop")),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "PolymorphSwiftGen",
-            dependencies: ["PolymorphGen"]),
+            dependencies: ["PolymorphGen", "SwiftCodeWriter"]),
         .testTarget(
             name: "PolymorphSwiftGenTests",
             dependencies: ["PolymorphSwiftGen"]),
--- Tests/PolymorphSwiftGenTests/PolymorphSwiftGenTests.swift
+++ Tests/PolymorphSwiftGenTests/PolymorphSwiftGenTests.swift
@@ -3,18 +3,6 @@ import XCTest
 
 class PolymorphSwiftGenTests: XCTestCase {
 
-    func testWriteEmptyFile() {
-        let fileDescription = SwiftFileDescription()
-        XCTAssertEqual("", SwiftFileWriter.default.write(fileDescription: fileDescription))
-    }
-
-    func testWriteOnlyDocumentationFile() {
-        let fileDescription = SwiftFileDescription(documentation: "SwiftPlatformGen.swift\nPolymorphSwiftGen\n\nCreated by Benoit BRIATTE on XX/XX/XXXX.")
-        XCTAssertEqual("//\n//  SwiftPlatformGen.swift\n//  PolymorphSwiftGen\n//\n//  Created by Benoit BRIATTE on XX/XX/XXXX.\n//\n", SwiftFileWriter.default.write(fileDescription: fileDescription))
-    }
-
-    static var allTests = [
-        ("testWriteEmptyFile", testWriteEmptyFile),
-        ("testWriteOnlyDocumentationFile", testWriteOnlyDocumentationFile)
+    static var allTests: [(String, (PolymorphSwiftGenTests) -> () -> ())] = [
     ]
 }
