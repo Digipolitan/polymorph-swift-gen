63dbf72721d202f2fa95898f4f98759b589eb244
Benoit BRIATTE <briatte.benoit@digipolitan.com>
2017-10-03 17:28:34 +0200

    update develop dependencies

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/Digipolitan/polymorph-gen.git", .branch("master")),
+        .package(url: "https://github.com/Digipolitan/polymorph-gen.git", .branch("develop")),
         .package(url: "https://github.com/Digipolitan/swift-code-writer.git", .branch("master")),
     ],
     targets: [
